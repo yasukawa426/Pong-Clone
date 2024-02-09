72b6864aee692267c640ecff0d255c7a2503c8cb
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-02-09 15:37:11 -0300

    Implementado a movimentação da bola.

---
+++ Scenes/Entities/ball.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+#Velocidade que a bola se meche
+@export var speed: float = 200
+
+var movement: Vector2 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	movement = Vector2(1, 1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += (movement.normalized() * speed) * delta
+	
+	print_debug("Posição da bola: " + str(position))
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -6,19 +6,41 @@ var first_score:int = 0
 #variavel que conta o score segundo
 var second_score:int = 0
 
+#Instancia do jogador 1
+var player1: Node
+
+#Instancia do jogador 2
+var player2: Node
+
+#Instancia da bola
+var ball: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# identifica qual jogador é qual
-	$Player1.is_player_one = true
-	$Player2.is_player_two = true
+	player1 = $Entities/Player1
+	player2 = $Entities/Player2
 	
-	print_debug("Player1: " + " " + str($Player1.player_size))
-	print_debug("Player2: " + " " + str($Player2.player_size))
+	ball = $Entities/Ball
+	
+	# identifica qual jogador é qual
+	player1.is_player_one = true
+	player2.is_player_two = true
+	
+	print_debug("Player1: " + " " + str(player1.player_size))
+	print_debug("Player2: " + " " + str(player2.player_size))
+	
+	start_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+
+#Método chamado para começar o jogo
+func start_game():
+	ball.position = $Markers/BallPosition.position
+	player1.position = $Markers/Player1Position.position
+	player2.position = $Markers/Player2Position.position
 
 #Acontece quando o jogador detecta alguma colisão com a bola
 func _on_player_body_entered(body):
@@ -27,7 +49,25 @@ func _on_player_body_entered(body):
 #Acontece quando uma das paredes detectam colisão com a bola
 func _on_wall_area_entered(area):
 	print_debug("Colisão detectada nas paredes")
+	
+	#pegamos o vetor de movimento da bola atual
+	var movement: Vector2 = ball.movement
+	
+	#vamos inverter apenas o eixo x
+	movement.x = movement.x * -1
+	
+	#atualizamos o valor na bola
+	ball.movement = movement
 
 #Acontece quando o teto ou chão detecta uma colisão com a bola
 func _on_ceiling_area_entered(area):
 	print_debug("Colisão detectada no teto/chão")
+	
+	#pegamos o vetor de movimento da bola atual
+	var movement: Vector2 = ball.movement
+	
+	#vamos inverter apenas o eixo y
+	movement.y = movement.y * -1
+	
+	#atualizamos o valor na bola
+	ball.movement = movement
