0ac484459d9181abef230b3ce2b00a9283d5ac34
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-02-09 10:50:22 -0300

    Agora carrega o tamanho do player e limita o movimento corretamente levando em conta o tamanho Formatação

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,12 +3,14 @@ extends Area2D
 #velocidade de movimento (pixel/sec)
 @export var speed: int = 600
 
+#tamanho do player
+var player_size: Vector2
 
-#booleans q dizem qual jogar é qual é atualizado no _ready() do node main
-#bool q diz se é o jogador 1
+#booleans q dizem qual jogar � qual � atualizado no _ready() do node main
+#bool q diz se � o jogador 1
 var is_player_one: bool = false
 
-#bool q diz se é o jogador 2
+#bool q diz se � o jogador 2
 var is_player_two: bool = false
 
 # tamanho da tela
@@ -17,38 +19,42 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size #carrega o tamanho da tela
+	screen_size = get_viewport_rect().size  #carrega o tamanho da tela
+
+	#carrega o tamanho do player (largura, altura) e multiplica pela escala dele
+	player_size = ($CollisionShape2D.get_shape().get_rect().size) * self.scale
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	#vetor de movimento
 	var movement: Vector2 = Vector2.ZERO
-	
+
 	#checar os inputs
-	
-	if (is_player_one):
+
+	if is_player_one:
 		if Input.is_action_pressed("move_up_1"):
 			movement.y -= 1
-			
+
 		if Input.is_action_pressed("move_down_1"):
 			movement.y += 1
-	
-	if (is_player_two):	
+
+	if is_player_two:
 		if Input.is_action_pressed("move_up_2"):
 			movement.y -= 1
-			
+
 		if Input.is_action_pressed("move_down_2"):
 			movement.y += 1
-	
+
 	# modificamos os valores do vetor com a velocidade
 	movement = movement * speed
-	
-	
-	#atualizamos a posição do player
+
+	#atualizamos a posi��o do player
 	position += movement * delta
-	
+
 	#limitamos o movimento pro player n sair da tela
-	position = position.clamp(Vector2(24, 24), screen_size)
-	
+	#min: (0 + metade da altura do player)
+	#Max: (altura da tela - meta da altura do player)
+	position = position.clamp(
+		Vector2(0, 0 + player_size.y / 2), Vector2(screen_size.x, screen_size.y - player_size.y / 2)
+	)
