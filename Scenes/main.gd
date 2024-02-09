5a332ebdadc53016a0c3ec3748d64381e98ea21b
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-02-09 10:07:13 -0300

    Agora é possível controlar cada jogador independentemente.

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -9,7 +9,9 @@ var second_score:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# identifica qual jogador é qual
+	$Player1.is_player_one = true
+	$Player2.is_player_two = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,9 +4,20 @@ extends Area2D
 @export var speed: int = 600
 
 
+#booleans q dizem qual jogar é qual é atualizado no _ready() do node main
+#bool q diz se é o jogador 1
+var is_player_one: bool = false
+
+#bool q diz se é o jogador 2
+var is_player_two: bool = false
+
+# tamanho da tela
+var screen_size
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size #carrega o tamanho da tela
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +27,20 @@ func _process(delta):
 	var movement: Vector2 = Vector2.ZERO
 	
 	#checar os inputs
-	if Input.is_action_pressed("move_up_1"):
-		movement.y -= 1
-		
-	if Input.is_action_pressed("move_down_1"):
-		movement.y += 1
-		
+	
+	if (is_player_one):
+		if Input.is_action_pressed("move_up_1"):
+			movement.y -= 1
+			
+		if Input.is_action_pressed("move_down_1"):
+			movement.y += 1
+	
+	if (is_player_two):	
+		if Input.is_action_pressed("move_up_2"):
+			movement.y -= 1
+			
+		if Input.is_action_pressed("move_down_2"):
+			movement.y += 1
 	
 	# modificamos os valores do vetor com a velocidade
 	movement = movement * speed
@@ -29,4 +48,7 @@ func _process(delta):
 	
 	#atualizamos a posição do player
 	position += movement * delta
+	
+	#limitamos o movimento pro player n sair da tela
+	position = position.clamp(Vector2(24, 24), screen_size)
 	
